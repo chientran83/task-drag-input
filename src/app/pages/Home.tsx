7faeb76be41ebbdacb5beaaf6730d8b618920829
Tran Van Chien <chientran868pro@gmail.com>
2023-01-11 11:39:44 +0700

    first commit app

---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "app/styles/GlobalStyle.scss"
+
+const Home : React.FC = () : React.ReactElement => {
+    return (
+        <div className="container">
+            
+        </div>
+    )
+}
+
+export default Home;
