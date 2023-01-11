1435a92c2cc561f7c9c1d7c8ee49660e91932275
truong0235x <maxphe0235@gmail.com>
2023-01-11 13:07:35 +0700

    layout division~

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from 'app/pages/Home';
+import "app/styles/GlobalStyle.scss"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
