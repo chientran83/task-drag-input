f5b7711abfc466fdc9298e9093204937b727f0df
truong0235x <maxphe0235@gmail.com>
2023-01-12 13:49:25 +0700

    custom Radio

---
+++ src/app/components/commons/Radio.tsx
@@ -0,0 +1,41 @@
+import { Form, RadioProps } from "antd";
+import { FC, ReactElement } from "react";
+import { Radio as RadioAntd } from "antd";
+import { Controller } from "react-hook-form";
+import { SelectOption } from "app/consts/types";
+
+type RadioPropsUseForm = RadioProps & {
+  label?: string;
+  name: string;
+  control: any;
+  rules?: object;
+  options?: SelectOption[];
+};
+
+const Radio: FC<RadioPropsUseForm> = ({
+  options,
+  label,
+  name,
+  control,
+  ...prop
+}): ReactElement => {
+  return (
+    <Form.Item label={label}>
+      <Controller
+        name={name}
+        render={({ field }) => (
+          <RadioAntd.Group {...field} {...prop}>
+            {options?.map((item: SelectOption) => (
+              <RadioAntd key={item.value} value={item?.value}>
+                {item.label}
+              </RadioAntd>
+            ))}
+          </RadioAntd.Group>
+        )}
+        control={control}
+      />
+    </Form.Item>
+  );
+};
+
+export default Radio;
